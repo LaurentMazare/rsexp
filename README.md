2a9fb5a98f4766f4e22ff94055ab66975ec77eb7
laurent <laurent.mazare@gmail.com>
2021-10-09 12:14:13 +0100

    Add some simple benchmark tool.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rsexp
 S-expression parsing and writing in Rust using [nom](https://github.com/Geal/nom) parser combinators.
+
+[![Build Status](https://github.com/LaurentMazare/rsexp/workflows/Continuous%20integration/badge.svg)](https://github.com/LaurentMazare/rsexp/actions)
+[![Latest version](https://img.shields.io/crates/v/rsexp.svg)](https://crates.io/crates/rsexp)
+[![Documentation](https://docs.rs/rsexp/badge.svg)](https://docs.rs/rsexp)
+![License](https://img.shields.io/crates/l/rsexp.svg)
---
+++ examples/sexp_utils.rs
@@ -0,0 +1,68 @@
+// TODO: use tokio?
+use clap::{AppSettings, Clap};
+use rsexp::Sexp;
+use tracing::{event, Level};
+
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Laurent Mazare <lmazare@gmail.com>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    Bench(Bench),
+}
+
+#[derive(Clap)]
+struct Bench {
+    #[clap(short, long)]
+    input_filename: String,
+
+    #[clap(short, long)]
+    output_filename: Option<String>,
+
+    #[clap(long, default_value = "1")]
+    iterations: u32,
+
+    #[clap(short)]
+    verbose: bool,
+}
+
+impl Bench {
+    fn run(&self) -> std::io::Result<()> {
+        event!(Level::INFO, "reading {}", self.input_filename);
+        let contents = std::fs::read(&self.input_filename)?;
+        event!(Level::INFO, "read {} bytes", contents.len());
+        let mut value = Sexp::List(vec![]);
+        for _index in 0..self.iterations {
+            value = rsexp::sexp(&contents).unwrap();
+        }
+        event!(Level::INFO, "converted to sexp {} times", self.iterations);
+        for _index in 0..self.iterations {
+            let _contents = value.to_bytes();
+        }
+        event!(Level::INFO, "converted to buffer {} times", self.iterations);
+        if let Some(output_filename) = &self.output_filename {
+            let data = value.to_bytes();
+            std::fs::write(output_filename, data)?;
+        }
+        Ok(())
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let opts: Opts = Opts::parse();
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    match opts.subcmd {
+        SubCommand::Bench(bench) => bench.run()?,
+    };
+    Ok(())
+}
